c9d05de5fa05842d942230c083cd1369575891ee
Stjepan Baričević <stjepan.baricevic@agrivi.com>
2020-10-01 22:42:33 +0200

    Project setup

---
+++ ConsoleApp1/ConsoleApp1/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public class Student
+    {
+        public virtual int ID { get; set; }
+        public virtual int LastName { get; set; }
+        public virtual int FirstMidName { get; set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var cfg = new Configuration();
+
+            String DataSource = asia13797\\sqlexpress;
+            String InitialCatalog = NHibernateDemoDB;
+            String IntegratedSecurity = True;
+            String ConnectTimeout = 15;
+            String Encrypt = false;
+            String TrustServerCertificate = False;
+            String ApplicationIntent = ReadWrite;
+            String MultiSubnetFailover = False;
+        }
+    }
+}
